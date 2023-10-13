c06719efed00d1b3f9347b3da9634fad98f02e86
thanhtri2112 <tri.nt.63cntt@ntu.edu.vn>
2023-10-13 17:44:35 +0700

    Tuan 5

---
+++ MyClass/DAO/CategoriesDAO.cs
@@ -0,0 +1,72 @@
+﻿using MyClass.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClass.DAO
+{
+    public class CategoriesDAO
+    {
+        private MyDBContext db = new MyDBContext();
+
+        //select * from
+        public List<Categories> getList()
+        {
+            return db.Categories.ToList();
+        }
+
+        //seclect * from cho index chi voi status 1 va 2
+        public List<Categories> getList(string status = "ALL")
+        {
+            List<Categories> list = null;
+            switch (status)
+            {
+                case "Index"://1,2
+                    {
+                        list = db.Categories.Where(m =>m.Status !=0).ToList();
+                        break;
+                    }
+                case "Trash"://0
+                    {
+                        list = db.Categories.Where(m => m.Status == 0).ToList();
+                        break;
+                    }
+                default:
+                    {
+                        list = db.Categories.ToList();
+                        break;
+                    }
+            }
+            return list;
+        }
+        public Categories getRow(int? id)
+        {
+            if(id == null)
+            {
+                return null;
+            }
+            else
+            {
+                return db.Categories.Find(id);
+            }
+        }
+        public int Insert(Categories row) 
+        {
+            db.Categories.Add(row);
+            return db.SaveChanges();
+        }
+        public int Update(Categories row)
+        {
+            db.Entry(row).State = EntityState.Modified;
+            return db.SaveChanges();
+        }
+        public int Delete(Categories row)
+        {
+            db.Categories.Remove(row);
+            return db.SaveChanges();
+        }
+    }
+}
--- MyClass/Model/Categories.cs
+++ MyClass/Model/Categories.cs
@@ -12,21 +12,33 @@ namespace MyClass.Model
     public class Categories
     {
         [Key]
+
         public int Id { get; set; }
         [Required]
+        [Display(Name="Tên loại SP")]
         public string Name { get; set; }
+        [Display(Name = "Tên rút gọn")]
         public string Slug { get; set; }
+        [Display(Name = "Cấp cha")]
         public int? ParentID { get; set; }
+        [Display(Name = "Sắp xếp")]
         public int? Order { get; set; }
 
         [Required]
+        [Display(Name = "Mô tả")]
         public string MetaDesc { get; set; }
         [Required]
+        [Display(Name = "Từ khóa")]
         public string MetaKey { get; set; }
+        [Display(Name = "Tạo bởi")]
         public int CreateBy { get; set; }
+        [Display(Name = "Ngày tạo")]
         public DateTime CreateAt { get; set; }
+        [Display(Name = "Cập nhật bởi")]
         public int UpdateBy { get; set; }
+        [Display(Name = "Ngày cập nhật")]
         public DateTime UpdateAt { get; set; }
+        [Display(Name = "Trạng thái")]
         public int Status { get; set; }
 
     }
