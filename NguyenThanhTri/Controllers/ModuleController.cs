607f157a64e30a02741edcc72f547b0eb7b95c63
thanhtri2112 <tri.nt.63cntt@ntu.edu.vn>
2023-12-02 18:27:32 +0700

    Tuan 12

---
+++ NguyenThanhTri/Controllers/ModuleController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyClass.DAO;
+namespace _63CNTT5N1.Controllers
+{
+    public class ModuleController : Controller
+    {
+        MenusDAO menusDAO = new MenusDAO();
+        ///////////////////////////////////////////////////////////////////
+        //GET: MainMenu
+        public ActionResult MainMenu()
+        {
+            return View(menusDAO.getListByParentId(0));
+        }
+    }
+
+}
